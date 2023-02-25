1300cafbc524bf24eb47d73f461bb86a68977032
HusanDadajonov <xusandadajonov4385@gmail.com>
2023-02-25 11:34:07 +0500

    add token

--- axios.js
+++ axios.js
@@ -10,7 +10,7 @@ if(typeof window !== "undefined"){
 const instance = axios.create({
   baseURL: baseUrl,
   headers: {
-    'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMjUxZTZiYzMtMTFjMi00NmYxLWFmZjEtMzA0MTg2NmU5N2ZkIiwidG9rZW5fdHlwZSI6ImFjY2VzcyIsImlhdCI6MTY3NjYyNjg0NCwiZXhwIjoxNjc3MjMxNjQ0fQ.MP0CxGTbZW3K1wI-OvbR8LyngZ2y10jLJKQ35wwDNxs` 
+    'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMjUxZTZiYzMtMTFjMi00NmYxLWFmZjEtMzA0MTg2NmU5N2ZkIiwidG9rZW5fdHlwZSI6ImFjY2VzcyIsImlhdCI6MTY3NzMwNjc5MiwiZXhwIjoxNjc3OTExNTkyfQ.xKYslHzXul0xb2mMKIy1cKTYXzwmb2IErvkGBhFX8zI` 
   }
 });
 
